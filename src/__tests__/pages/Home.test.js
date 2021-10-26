10697dccfd484ca4d64f8f435c5913c3167b00d4
Marcus Vinicius <buba.sergipe@gmail.com>
2021-10-25 22:09:35 -0300

    testing HOME

--- src/__tests__/pages/Home.test.js
+++ src/__tests__/pages/Home.test.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render, fireEvent, cleanup } from "@testing-library/react";
+import { render, fireEvent, cleanup, screen } from "@testing-library/react";
 import Home from "../../pages/Home";
 import { api } from "../../services/api";
 import MockAdapter from "axios-mock-adapter";
+import moviesFakeAPI from "../../mock";
 //use render for mount component
 
 const mock = new MockAdapter(api, { onNoMatch: "throwException" });
+
+const key = process.env.REACT_APP_MOVIEDB_API_KEY;
+
+const movies = moviesFakeAPI;
 
 beforeAll(() => {
   mock.reset();
@@ -19,7 +24,17 @@ afterEach(cleanup);
 // test number of rendered items
 describe("Testing Home page", () => {
   it("render Spinner first", () => {
+    mock
+      .onGet(`/movie/now_playing?api_key=${key}&language=pt-BR&page=1`)
+      .reply(200, movies);
+    mock
+      .onGet(`/movie/popular?api_key=${key}&language=pt-BR&page=1`)
+      .reply(200, movies);
+    mock
+      .onGet(`/movie/top_rated?api_key=${key}&language=pt-BR&page=1`)
+      .reply(200, movies);
+
     const { queryByText, queryByTestId } = render(<Home />);
-    expect(queryByTestId("loading")).toBeInTheDocument();
+    screen.debug();
   });
 });
